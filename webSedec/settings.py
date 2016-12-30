8cf29511281e190d5d52eb000c6c740af56c10df
Felipe Henrique de Almeida Bormann <fbormann@Felipes-MacBook-Air.local>
2016-12-30 01:10:21 -0300

    modifying packages for static files on django

--- webSedec/settings.py
+++ webSedec/settings.py
@@ -123,6 +123,29 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 try:
     from .local_settings import *
 except ImportError:
